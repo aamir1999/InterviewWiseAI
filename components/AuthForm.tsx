055fe9a112ec81e21bd09f3aeaa8c77f1f7db2f3
Aamir <amallick546@gmail.com>
2025-08-03 22:31:59 +0530

    Final

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -105,7 +105,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
           <h2 className="text-primary-100">InterGenixAI</h2>
         </div>
 
-        <h3>Practice job interviews with AI</h3>
+        <h3>Practice job interviews with AI tailored to your role, in Real-Time.</h3>
 
         <Form {...form}>
           <form
